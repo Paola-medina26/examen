b6c868e5f67d8fe352627206b22013cdcb353441
Paola-medina26 <65326378+Paola-medina26@users.noreply.github.com>
2020-06-03 19:46:35 -0500

    Update PintoresController.js

--- PintoresController.js
+++ PintoresController.js
@@ -1,24 +1,20 @@
-let mongoose = require('mongoose');
-let Pintores = require('../models/Pintores');
+let mongoose = require ('mongoose');
+let Pintores = require ('../models/Pintores');
+let pintoresController ={};
 
-let PintoresController = {};
-
-/*LISTAR*/
-PintoresController.list = (req, res)=>{
-    Pintores.find({})
-        .limit(20)
-        .skip(num:0)
-        .exec((err, pintor)=>{
-            if (err){
-                console.log('Error:',err);
-            }
-            
-            res.render(view:'../views/index', options:{
-                pintores: pintor,
-                 titulo: "Listado de pintores",
-                year: new Date().getDate()
-            });
-        })
+pintoresController.list=(req, res)=>{
+  Pintores.find({})
+  .limit(20)
+  .skip(num:0)
+  .exec(callback:(err, pintor)=>{
+    if(err){
+      console.log('Error: ', err)
+    }
+    res.render(view: '../views/index', options:{
+      pintores:pintor,
+      titulo:"Listado de pintores",
+      year: new Date().getDate()
+    })
+  })
 };
-
-module.exports = PintoresController;
+module.exports = pintoresController;
