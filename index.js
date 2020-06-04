490a5917651e4e2232d22053b24eea7e5ea2ca25
Paola-medina26 <65326378+Paola-medina26@users.noreply.github.com>
2020-06-03 19:49:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const express = require('express');
+const express= require('express');
 const hbs = require('hbs');
-const mongoose = require('mongoose');
+const mongoose = require ('mongoose');
 
-const port = process.env.PORT || 3000;
-let pintoresRouter = require('./routes/pintor');
+const PUERTO = process.env.PORT||3000;
 
-const app = express();
+let pintoresRouter= require('./routes/pintor');
 
-app.set('view engine','hbs');
-
-hbs.registerPartials(__dirname + '/views/partials');
-//sitio estatico
+const app =express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+ '/views/partials');
 app.use(express.static(__dirname+'/public'));
-app.use('/pintor',pintoresRouter);
 
+app.use('/', pintoresRouter);
 
 mongoose.Promise = global.Promise;
-const cadena = 'mongodb+srv://leslypaola:1213141516@medinalesly4-g-svzi3.mongodb.net/<dbname>?retryWrites=true&w=majority';
-mongoose.connect(cadena,{useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=>{
-        console.log('Conexión establecida =oD');
-    })
-    .catch(err=> console.log(err));
+const cadena='mongodb+srv://leslypaola:1213141516@medinalesly4-g-svzi3.mongodb.net/curso?retryWrites=true&w=majority'
 
+mongoose.connect(cadena,{useNewUrlParser: true, useUnifiedToology: true})
 
-app.listen(PUERTO,()=>{
-    console.log('Escuchando el puerto....');
+.then(()=>{
+  console.log('Conexion establecida=oD')
+}).catch(err=>console.log(err));
+
+app.listen(PUERTO, ()=>{
+  console.log('Escuchando el puerto...');
 });
